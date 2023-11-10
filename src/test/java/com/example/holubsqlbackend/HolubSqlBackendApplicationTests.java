2d1bb1d225cee70c7e989d4a113835c1fab24b09
rineeee <harin14@naver.com>
2023-11-10 14:39:20 +0900

    first commit

---
+++ src/test/java/com/example/holubsqlbackend/HolubSqlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holubsqlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolubSqlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
