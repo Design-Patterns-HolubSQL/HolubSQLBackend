cb7dd19f5804fdd51888f2e92c94576e15d4c30d
rineeee <harin14@naver.com>
2023-11-10 17:06:06 +0900

    feat: get method example

--- src/main/java/com/example/holubsqlbackend/domain/service/restaurantService.java
+++ src/main/java/com/example/holubsqlbackend/domain/service/restaurantService.java
@@ -1,4 +1,12 @@
 package com.example.holubsqlbackend.domain.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class restaurantService {
+    public String getRestaurant() {
+        return "restaurant1";
+    }
 }
